71f1a6e055b2ce07346a59678be247052165a53b
dwioktaviani <dwi.oktaviani@basajans.com>
2016-02-03 16:56:01 +0700

    upload file IStudentsRepository.cs

---
+++ StudentRESTService/IStudentsRepository.cs
@@ -0,0 +1,22 @@
+﻿using StudentRESTService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRESTService
+{
+    public interface IStudentsRepository 
+    {
+        List<Students> GetAllStudents();
+
+        Students GetStudentsId(int Id);
+
+        Students AddNewStudents(Students item);
+
+        bool DeleteAStudents(int Id);
+
+        bool UpdateAStudents(Students item);
+    }
+}
