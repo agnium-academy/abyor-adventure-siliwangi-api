fd6838b62806046f7a131c467e17de2f8b20589e
sofifitriyah <sofi.fitriyah@basajans.com>
2016-02-03 18:22:15 +0700

    hapus using di studentsrepository.cs yang ga perlu

--- StudentRESTService/StudentsRepository.cs
+++ StudentRESTService/StudentsRepository.cs
@@ -1,9 +1,6 @@
-﻿using StudentRESTService;
-using StudentRESTService.Model;
+﻿using StudentRESTService.Model;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace StudentRESTService
 {
