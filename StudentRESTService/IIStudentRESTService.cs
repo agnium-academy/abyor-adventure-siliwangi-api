d2b179af2b079c205637d0d184e02a40f1d5f3cc
ucunurul <ucu.nurul.ulum@basajans.com>
2016-02-03 15:19:49 +0700

    Nambahkan Interface IStudentREstService

---
+++ StudentRESTService/IIStudentRESTService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using StudentRESTService.Model;
+
+namespace StudentRESTService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IIStudentRESTService" in both code and config file together.
+    [ServiceContract]
+    public interface IIStudentRESTService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json,
+                                  BodyStyle = WebMessageBodyStyle.Bare,
+                                  UriTemplate = "GetStudentList/")]
+        List<Students> GetStudentList();
+        //void DoWork();
+    }
+}
