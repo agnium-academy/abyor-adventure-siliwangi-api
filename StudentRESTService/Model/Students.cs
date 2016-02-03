9adc127c9fc2f2ef4db0db8cf62a10c4227d087a
ucunurul <ucu.nurul.ulum@basajans.com>
2016-02-03 15:06:50 +0700

    Membuat Model Students

---
+++ StudentRESTService/Model/Students.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace StudentRESTService.Model
+{
+    [DataContract]
+    public class Students
+    {
+        [DataMember]
+        public int NIM { get; set; }
+
+        [DataMember]
+        public string Nama { get; set; }
+
+        [DataMember]
+        public string Alamat { get; set; }
+
+        [DataMember]
+        public int Umur { get; set; }
+    }
+}
