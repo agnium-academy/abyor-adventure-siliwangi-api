0afe598f286b1b50f584466a484728c951ea602d
ucunurul <ucu.nurul.ulum@basajans.com>
2016-02-03 15:31:09 +0700

    Nambahin file Interface Students

--- StudentRESTService/IStudentRESTServices.cs
+++ StudentRESTService/IStudentRESTServices.cs
@@ -1,23 +1,22 @@
-﻿using System;
+﻿using StudentRESTService.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
-using StudentRESTService.Model;
 
 namespace StudentRESTService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IIStudentRESTService" in both code and config file together.
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IIStudentRESTServices" in both code and config file together.
     [ServiceContract]
-    public interface IIStudentRESTService
+    public interface IStudentRESTServices
     {
         [OperationContract]
         [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json,
                                   BodyStyle = WebMessageBodyStyle.Bare,
                                   UriTemplate = "GetStudentList/")]
         List<Students> GetStudentList();
-        //void DoWork();
     }
 }
---
+++ StudentRESTService/StudentRESTService.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using StudentRESTService.Model;
+
+namespace StudentRESTService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "StudentRESTService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select StudentRESTService.svc or StudentRESTService.svc.cs at the Solution Explorer and start debugging.
+    public class StudentRESTService : IStudentRESTService
+    {
+        public List<Students> GetStudentList()
+        {
+            return StudentList.Instance.studentlist;
+        }
+    }
+}
